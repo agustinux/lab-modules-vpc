7886cc6973703741928d787b62907db68bd25c53
AgusTinux <agarcia71@gmail.com>
2024-09-13 15:48:22 -0500

    Agregamos mas archivos

---
+++ voxia-website-ecs/main.tf
@@ -0,0 +1,20 @@
+# Configure AWS provider
+
+provider "AWS" {
+  region  = var.region
+  profile = "terraform-user"
+}
+
+# Create VPC
+module "aws_vpc" {
+  source                       = "../modules/vpc"
+  region                       = var.region
+  project_name                 = var.project_name
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
